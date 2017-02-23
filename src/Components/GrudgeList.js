bed57167c4802026135720779243da409149d74b
MilkMan90 <mlkaufman747@gmail.com>
2017-02-23 13:53:57 -0700

    sort by name added and functioning

--- src/Components/GrudgeList.js
+++ src/Components/GrudgeList.js
@@ -3,16 +3,55 @@ import SingleGrudge from './SingleGrudge'
 import '../style/List.css';
 
 class GrudgeList extends Component {
+  constructor(){
+    super()
+    this.state = {
+      nameSort: -1,
+      dateSort: 'newest'
+    }
+  }
+  sortByName(grudges){
+      return grudges.sort((a, b)=>{
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+          return this.state.nameSort;
+        }
+        if (nameA > nameB) {
+          return -this.state.nameSort;
+        }
+        return 0;
+      })
+    }
+  sortByDate(grudges){
+
+  }
+  toggleSortByName(){
+      this.setState({
+        nameSort: this.state.nameSort * -1
+      })
+  }
+  toggleSortByDate(){
+
+  }
   render() {
-    let grudgeArray;
+    let grudgeList;
     if(this.props.grudges){
-      grudgeArray = this.props.grudges.map((grudge)=>{
+
+      let {grudges} = this.props
+      //sort by name
+      grudges = this.sortByName(grudges)
+      //sort by date
+
+      grudgeList = grudges.map((grudge)=>{
         return <SingleGrudge key={grudge.id} grudge={grudge} showGrudge={this.props.showGrudge}/>
       })
     }
     return (
       <ul className="GrudgeList">
-        {grudgeArray}
+        <button onClick={()=>this.toggleSortByDate()}>Sort By Date</button>
+        <button onClick={()=>this.toggleSortByName()}>Sort By Name</button>
+        {grudgeList}
       </ul>
     );
   }
